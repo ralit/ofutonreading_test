9b56bca1596ebea40a0bde6654dd23fcd5287aae
ralit <ralit@ralitMacBookProMid2012.local>
2013-11-21 18:29:38 +0900

    zip

--- src/org/ralit/bookbrainstall/MainActivity.java
+++ src/org/ralit/bookbrainstall/MainActivity.java
@@ -1,5 +1,6 @@
 package org.ralit.bookbrainstall;
 
+import java.io.BufferedOutputStream;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
@@ -13,6 +14,8 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Stack;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
 import jp.recognize.HttpSceneryLineLayoutAnalysisRequest;
 import jp.recognize.SceneryLineLayoutAnalyzer;
@@ -615,6 +618,40 @@ public class MainActivity extends Activity implements AnimatorListener, FileOpen
         ispos = true;
 }
 
+    private void openZip() {
+    	Log.i(tag, "openZip()");
+    	try {
+			ZipInputStream zis = new ZipInputStream(new FileInputStream(filepath));
+			ZipEntry ze;
+			BufferedOutputStream bos;
+			int len;
+			
+			while ((ze = zis.getNextEntry()) != null) {
+				File file = new File(ze.getName());
+				File tmpdir = new File(Environment.getExternalStorageDirectory().getPath() + "/imagemove/tmp_zip");
+				try {
+					if (!tmpdir.exists()) { tmpdir.mkdir(); }
+				} catch (SecurityException e) {
+					e.printStackTrace();
+				}
+				bos = new BufferedOutputStream(new FileOutputStream(Environment.getExternalStorageDirectory().getAbsolutePath() + "/imagemove/tmp_zip/" + file.getName()));
+				byte[] buffer = new byte[1024];
+				while ((len = zis.read(buffer)) != -1) { bos.write(buffer, 0, len); }
+				zis.closeEntry();
+				bos.close();
+				bos = null;
+				
+				File dir = new File(Environment.getExternalStorageDirectory().getAbsolutePath() + "/imagemove/tmp_zip");
+				File[] fileList = dir.listFiles();
+				filepath = fileList[0].getAbsolutePath();
+				openImage();
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+    }
 
 	private void docomo () {
 		Log.i(tag, "docomo()");
@@ -628,7 +665,7 @@ public class MainActivity extends Activity implements AnimatorListener, FileOpen
 		} else if (filepath.endsWith(".jpg")) {
 			openImage();
 		} else if (filepath.endsWith(".zip")) {
-//			openZip();
+			openZip();
 		}
 //		openResource();
 		
